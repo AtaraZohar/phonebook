59b2de54d93775791f5c84489bd8ad61f2ed754f
atarazohar <atara@aniview.com>
2024-09-22 20:07:04 +0300

    fix

--- routes/contact_routes.go
+++ routes/contact_routes.go
@@ -9,7 +9,7 @@ import (
 func SetupRoutes(router *gin.Engine, contactController *controllers.ContactController) {
 	group := router.Group("/contacts")
 	group.POST("/", contactController.CreateContact)
-	group.GET("", contactController.GetContacts)
+	group.GET("/", contactController.GetContacts)
 	group.GET("/search", contactController.SearchContacts)
 	group.PUT("/:id", contactController.UpdateContact)
 	group.DELETE("/:id", contactController.DeleteContact)
