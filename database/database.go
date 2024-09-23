5ef2b0291182bdb5b85c3b3c2d9b7662a640de91
atarazohar <atara@aniview.com>
2024-09-23 21:03:37 +0300

    change database config

--- database/database.go
+++ database/database.go
@@ -8,7 +8,7 @@ import (
 var DB *gorm.DB
 
 func Connect() (*gorm.DB, error) {
-	dsn := "host=db user=myuser password=mypassword dbname=mydatabase port=5432 sslmode=disable"
+	dsn := "host=db user=atara password=mypassword dbname=phonebook port=5432 sslmode=disable"
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
